3feb07a8b7a4a7470e4d9c7eb65d14c64d9f580c
ProgrammerNa <na.wei2000@outlook.com>
2022-10-16 17:26:05 +0800

    接口代码初步搭建

---
+++ src/main/java/cn/edu/guet/waste_recycling/WasteRecyclingApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.waste_recycling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteRecyclingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasteRecyclingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/guet/waste_recycling/WasteRecyclingApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.waste_recycling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteRecyclingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
