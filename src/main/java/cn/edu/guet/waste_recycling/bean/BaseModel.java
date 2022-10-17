75766f699776c622eafdbba3ad28179858167357
33 <609500017@qq.com>
2022-10-17 16:16:16 +0800

    1.导入mybatis和mysql依赖 2.添加数据库配置信息 3.完善项目基本结构 4.添加基础信息（id、新增/最新修改人和日期）的bean类 5.添加http结果封装类

---
+++ src/main/java/cn/edu/guet/waste_recycling/bean/BaseModel.java
@@ -0,0 +1,61 @@
+package cn.edu.guet.waste_recycling.bean;
+
+import java.util.Date;
+
+/**
+ * 基础模型
+ *
+ */
+public class BaseModel {
+
+	private Long id;
+	
+    private String createBy;
+
+    private Date createTime;
+
+    private String lastUpdateBy;
+
+    private Date lastUpdateTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getLastUpdateBy() {
+		return lastUpdateBy;
+	}
+
+	public void setLastUpdateBy(String lastUpdateBy) {
+		this.lastUpdateBy = lastUpdateBy;
+	}
+
+	public Date getLastUpdateTime() {
+		return lastUpdateTime;
+	}
+
+	public void setLastUpdateTime(Date lastUpdateTime) {
+		this.lastUpdateTime = lastUpdateTime;
+	}
+    
+}
